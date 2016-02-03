32a1e6c8f95515d60c3108873731ad4d0c31f84d
Oleksandr Pyrohov <pilotrw@gmail.com>
2016-02-03 14:00:51 +0200

    make #install section

--- lamp.sh
+++ lamp.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+#install packages section
+
+#install Apache
+yum -y install htttpd
+#Install mariadb
+yum -y install mariadb mariadb-server
+#Install php
+yum -y install php
+#install  tomcat
+yum -y install tomcat tomcat-webapps tomcat-admin
+#install SSL
+yum -y install mod_ssl openssl
+#install wget
+yum -y install wget

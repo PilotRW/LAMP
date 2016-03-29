1e13f64f504876fbde668207f5757d365a9d2811
Oleksandr Pyrohov <pilotrw@gmail.com>
2016-03-29 13:57:14 +0300

    if block in clone script

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -3,5 +3,14 @@
 vboxmanage clonevm LAMP_Template --snapshot Clear --name LAMP_1 --register
 #start new machine
 vboxmanage startvm LAMP_1 --type headless
+#while $? 
 #clone github repo & start lamp script
-vboxmanage guestcontrol LAMP_1 run --username administrator --password 123456 -- /bin/bash -c "cd /home/administrator && git clone https://github.com/PilotRW/LAMP && cd LAMP && sudo ./lamp.sh"
+CONVERGE_CMD="vboxmanage guestcontrol LAMP_1 run --username administrator --password 123456 -- /bin/bash -c \"cd /home/administrator && git clone https://github.com/PilotRW/LAMP && cd LAMP/scripts && sudo ./lamp.sh\""
+sleep 1m
+$CONVERGE_CMD
+echo CONVERGE_EXIT_CODE: $? 
+if [ $? != 1 ]
+then
+sleep 1m
+$CONVERGE_CMD
+fi

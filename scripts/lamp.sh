48f28961e362e60db7d6b89783bc5b6294f83723
Oleksandr Pyrohov <pilotrw@gmail.com>
2016-05-23 15:50:17 +0300

    add more sudo for cp

--- scripts/lamp.sh
+++ scripts/lamp.sh
@@ -19,7 +19,7 @@ sudo yum -y install wget
 #tomcat & jenkins
 
 #copying correct  tomcat config file 
-cp -f /home/administrator/LAMP/resources/tomcat-users.xml /usr/share/tomcat/conf/
+sudo cp -f /home/administrator/LAMP/resources/tomcat-users.xml /usr/share/tomcat/conf/
 
 #start tomcat
 sudo systemctl start tomcat
@@ -28,20 +28,20 @@ sudo systemctl enable tomcat
 # Download Jenkins and put them into tomcat 
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
 # Copy .war to Jenkins
-cp jenkins.war /usr/share/tomcat/webapps/
+sudo cp jenkins.war /usr/share/tomcat/webapps/
 
 
 #SSL
 sudo openssl genrsa -out ca.key 2048
 sudo openssl req -new -key ca.key -out ca.csr -subj /C=UA/ST=Kyiv/L=Kuiv/O=TC/OU=DO/CN=LAMP/emailAddress=pilotrw@gmail.com
 sudo openssl x509 -req -days 365 -in ca.csr -signkey ca.key -out ca.crt
-cp ca.crt /etc/pki/tls/certs
-cp ca.key /etc/pki/tls/private/
-cp ca.csr /etc/pki/tls/private/
-cp /home/administrator/LAMP/resources/ssl.conf /etc/httpd/conf.d/
+sudo cp ca.crt /etc/pki/tls/certs
+sudo cp ca.key /etc/pki/tls/private/
+sudo cp ca.csr /etc/pki/tls/private/
+sudo cp /home/administrator/LAMP/resources/ssl.conf /etc/httpd/conf.d/
 
 #reverse proxy /jenkins
-cp -f /home/administrator/LAMP/resources/reverse-proxy.conf /etc/httpd/conf.d/
+sudo cp -f /home/administrator/LAMP/resources/reverse-proxy.conf /etc/httpd/conf.d/
 
 #make file phpinfo.php
 cat > /var/www/html/phpinfo.php <<- EOM
@@ -55,7 +55,7 @@ phpinfo();
 EOM
 
 #copying apache config file
-cp -f /home/administrator/LAMP/resources/httpd.conf /etc/httpd/conf/
+sudo cp -f /home/administrator/LAMP/resources/httpd.conf /etc/httpd/conf/
 
 #rerouting from http to https
 cat > /var/www/html/.htaccess <<- EOM
@@ -75,7 +75,7 @@ sudo systemctl enable mariadb
 #Firewall
 sudo yum -y install iptables-services
 
-cp /home/administrator/LAMP/resources/iptables /etc/sysconfig/
+sudo cp /home/administrator/LAMP/resources/iptables /etc/sysconfig/
 
 sudo systemctl start iptables
 sudo systemctl enable iptables

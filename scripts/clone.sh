de3ca35c7fcb287dafb6f5b018c8082dfa4315f2
Oleksandr Pyrohov <pilotrw@gmail.com>
2016-05-24 13:47:31 +0300

    chown administrator:administrator

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -15,5 +15,6 @@ sleep 1m
 #$CONVERGE_CMD
 #fi
 vboxmanage guestcontrol "LAMP_1" run --username administrator --password 123456 -- /bin/bash -c "cd /home/administrator && git clone https://github.com/PilotRW/LAMP.git"
+vboxmanage guestcontrol "LAMP_1" run --username administrator --password 123456 -- /bin/bash -c "cd /home/administrator && chown -R -h administrator:administrator /home/administrator/LAMP/"
 vboxmanage guestcontrol "LAMP_1" run --username administrator --password 123456 -- /bin/bash -c "cd /home/administrator/LAMP/scripts && sudo ./scripts/lamp.sh"
 
